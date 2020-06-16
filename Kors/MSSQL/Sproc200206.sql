0091f2e6d80b8262301d9c8a06be8f327019962e
Brent Groves <brent.groves@gmail.com>
2020-06-16 08:11:54 -0400

    Added/Changed Code

--- Kors/MSSQL/Sproc200206.sql
+++ Kors/MSSQL/Sproc200206.sql
@@ -48,18 +48,15 @@ SET NOCOUNT ON;
 IF OBJECT_ID(@table_name) IS NOT NULL
 	EXEC ('DROP Table ' + @table_name)
 
--- /* TESTING ONLY
+/* TESTING ONLY
 DECLARE @start_date DATETIME,
 	@end_date DATETIME,
 	@table_name varchar(12),
 	@record_count INT
--- set @start_date ='2019-12-31T00:00:00';  -- week 53
--- set @start_date ='2020-01-01T00:00:00';  -- week 1 
--- set @end_date ='2020-01-05T23:59:59';  -- week 2
-set @start_date ='2020-01-03T00:00:00';
+set @start_date ='2020-02-09T00:00:00';
 set @end_date ='2020-02-15T23:59:59';
 set @table_name = 'rpt0213test'
--- */ -- END TESTING ONLY
+*/ -- END TESTING ONLY
 	
 Declare @start_year char(4)
 Declare @start_week int
@@ -72,14 +69,13 @@ set @start_year = DATEPART(YEAR,@Start_Date)
 set @start_week = DATEPART(WEEK,@Start_Date)
 set @end_year = DATEPART(YEAR,@End_Date)
 set @end_week = DATEPART(WEEK,@End_Date)
--- select @start_year,@start_week,@end_year,@end_week;
+
 
 set @start_of_week_for_start_date = DATEADD(wk, DATEDIFF(wk, 6, '1/1/' + @start_year) + (@start_week-1), 6)  --start of week
 set @end_of_week_for_end_date = DATEADD(wk, DATEDIFF(wk, 5, '1/1/' + @end_year) + (@end_week-1), 5)  --end of week
 
 set @end_of_week_for_end_date = DATEADD(day, 1, @end_of_week_for_end_date);
 set @end_of_week_for_end_date = DATEADD(second,-1,@end_of_week_for_end_date);
-select @start_year,@start_week,@end_year,@end_week,@start_of_week_for_start_date,@end_of_week_for_end_date;
 
 /* may be necessary if multiple calls are done on the same connection
 decdrop table #resultslare @sqlDropPK nvarchar(4000)
@@ -183,7 +179,7 @@ create table #results
   actual_vrs_planned_percent varchar(10),
   scrap_count varchar(10),
   scrap_percent varchar(10),
-  downtime_minutes varchar(10)
+  downtime_minutes varchar(20)
 )
 
 insert into #results (primary_key,start_week,end_week,part_number,workcenter_code,planned_production_count,actual_production_count,actual_vrs_planned_percent,scrap_count,scrap_percent,downtime_minutes)
