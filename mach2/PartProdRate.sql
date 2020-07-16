ed094f2ebc52220b3f6841183dbbf7178246aeb4
Brent Groves <brent.groves@gmail.com>
2020-07-16 15:33:37 -0400

    Added/Changed Code

--- mach2/PartProdRate.sql
+++ mach2/PartProdRate.sql
@@ -2,31 +2,70 @@
 -- truncate table PartProdRate
 create table PartProdRate
 (
+  PartProdRate_Key INT NOT NULL AUTO_INCREMENT, 
   part_key long,
   part_no varchar (113),  
   period int,
   start_date datetime,
   end_date datetime,
-  quantity decimal (19,5)	
-)
-exitJ
+  quantity decimal (19,5),	
+  PRIMARY KEY (PartProdRate_Key)  
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-/*
-LOAD DATA INFILE '/var/lib/mysql-files/EdonLE5000.csv' INTO TABLE PlxEdonSupplyItem0615 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/var/lib/mysql-files/EdonLE10000.csv' INTO TABLE PlxEdonSupplyItem0615 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/var/lib/mysql-files/EdonLE15000.csv' INTO TABLE PlxEdonSupplyItem0615 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/var/lib/mysql-files/EdonLE20000.csv' INTO TABLE PlxEdonSupplyItem0615 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/var/lib/mysql-files/EdonLE25000.csv' INTO TABLE PlxEdonSupplyItem0615 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/var/lib/mysql-files/EdonGT25000.csv' INTO TABLE PlxEdonSupplyItem0615 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
--- If importing into MySQL db container use the following
-LOAD DATA INFILE '/EdonLE5000.csv' INTO TABLE PlxEdonSupplyItem0620 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/EdonLE10000.csv' INTO TABLE PlxEdonSupplyItem0620 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/EdonLE15000.csv' INTO TABLE PlxEdonSupplyItem0620 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/EdonLE20000.csv' INTO TABLE PlxEdonSupplyItem0620 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/EdonLE25000.csv' INTO TABLE PlxEdonSupplyItem0620 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/EdonGT25000.csv' INTO TABLE PlxEdonSupplyItem0620 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-*/
 
 -- LOAD DATA INFILE '/prod0.csv' INTO TABLE PartProdRate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/prod0.csv' INTO TABLE PartProdRate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA INFILE '/prod0.csv' INTO TABLE PartProdRate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS (part_key,part_no,period,start_date,end_date,quantity);
+
+LOAD DATA INFILE '/prod0.csv'
+INTO TABLE PartProdRate 
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(part_key,part_no,period,start_date,end_date,quantity);
+
 SELECT * from PartProdRate
+
+set @startPeriod =  0;
+set @endPeriod = 4;
+select count(*) 
+into @pRecordCount
+from PartProdRate ppr where period between @startPeriod and @endPeriod ORDER BY PartProdRate_Key;
+select @pRecordCount;
+
+
+set @startPeriod =  0;
+set @endPeriod = 4;
+call PartProdRateFetch(@startPeriod,@endPeriod,90,0, @rec); 
+select @rec;
+-- drop procedure PartProdRateFetch
+CREATE DEFINER=`brent`@`%` PROCEDURE PartProdRateFetch (
+	pStartPeriod int,
+	pEndPeriod int,
+	pLimit int,
+	pSkip int,
+	OUT pRecordCount INT 
+)
+BEGIN
+
+	select 
+	PartProdRate_Key,
+	part_key,
+	part_no,
+	period,
+	date_format(start_date, '%Y-%m-%d %H:%i') start_date,
+	date_format(end_date, '%Y-%m-%d %H:%i') end_date,
+	Quantity
+	from PartProdRate ppr where period between pStartPeriod and pEndPeriod ORDER BY PartProdRate_Key LIMIT pLimit OFFSET pSkip; 
+	-- select * from CompareContainer where transDate between pStartDate and pEndDate ORDER BY CompareContainer_Key LIMIT pLimit OFFSET pSkip;  
+	-- select @pRecordCount := count(*) from CompareContainer where transDate between pStartDate and pEndDate;
+	-- set pRecordCount = @pRecordCount;
+	select count(*) 
+	into pRecordCount
+	from PartProdRate ppr 
+	where period between pStartPeriod and pEndPeriod;	
+
+   	-- SELECT ROW_COUNT(); -- 0
+   	-- set pRecordCount = FOUND_ROWS();
+end;
+
+
