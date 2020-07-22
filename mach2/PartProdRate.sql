ebb0a51b700326adc0fb60d11a5dcca64ce749a8
Brent Groves <brent.groves@gmail.com>
2020-07-22 15:41:26 -0400

    Added/Changed Code

--- mach2/PartProdRate.sql
+++ mach2/PartProdRate.sql
@@ -8,22 +8,25 @@ create table PartProdRate
   period int,
   start_date datetime,
   end_date datetime,
-  quantity decimal (19,5),	
+  quantity int,
+  rate decimal (9,2),	
   PRIMARY KEY (PartProdRate_Key)  
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 -- LOAD DATA INFILE '/prod0.csv' INTO TABLE PartProdRate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-LOAD DATA INFILE '/prod0.csv' INTO TABLE PartProdRate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS (part_key,part_no,period,start_date,end_date,quantity);
+LOAD DATA INFILE '/prod0.csv' INTO TABLE PartProdRate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS (part_key,part_no,period,start_date,end_date,quantity,rate);
 
 LOAD DATA INFILE '/prod0.csv'
 INTO TABLE PartProdRate 
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(part_key,part_no,period,start_date,end_date,quantity);
+(part_key,part_no,period,start_date,end_date,quantity,rate);
 
-SELECT quantity,format(quantity/480,2),FORMAT(quantity, 2) from PartProdRate
+select * from PartProdRate ppr 
+order by end_date desc
+
 
 set @startPeriod =  0;
 set @endPeriod = 4;
@@ -54,8 +57,9 @@ BEGIN
 	period,
 	date_format(start_date, '%Y-%m-%d %H:%i') start_date,
 	date_format(end_date, '%Y-%m-%d %H:%i') end_date,
-	FORMAT(Quantity, 2) Quantity,
-	format(Quantity/480,2) ProdRate
+	Quantity,
+	Rate
+	-- format(Quantity/480,2) ProdRate
 	from PartProdRate ppr where period between pStartPeriod and pEndPeriod ORDER BY PartProdRate_Key LIMIT pLimit OFFSET pSkip; 
 	-- select * from CompareContainer where transDate between pStartDate and pEndDate ORDER BY CompareContainer_Key LIMIT pLimit OFFSET pSkip;  
 	-- select @pRecordCount := count(*) from CompareContainer where transDate between pStartDate and pEndDate;
