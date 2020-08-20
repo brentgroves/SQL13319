f76ede23ecb528fd55f5753e337559f061eb1560
Brent Groves <brent.groves@gmail.com>
2020-08-20 11:04:26 -0400

    Added/Changed Code

--- mach2/ToolTracker.sql
+++ mach2/ToolTracker.sql
@@ -14,20 +14,57 @@ values (2809196,'Honda Civic CNC 359 362','Honda Civic Knuckle LH')
 
 select * from Workcenter tt 
 
+
+-- drop table CNC_Type 
+CREATE TABLE CNC_Type (
+	CNC_Type_Key int NOT NULL,
+	CNC_Type varchar(50) NOT NULL,
+  	PRIMARY KEY (CNC_Type_Key)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='CNC types such as Makino, Okuma, Welles, etc.';
+insert into CNC_Type (CNC_Type_key,CNC_Type)
+-- values (1,'Okuma')
+values (2,'Makino')
+select * from CNC_Type
+
+/*
+ * CNC info, don't want to have a varchar primary_key
+ */
+-- drop table CNC 
+CREATE TABLE CNC (
+	CNC_Key int NOT NULL,
+	CNC varchar(10) NOT NULL,
+	CNC_TYPE_Key int NOT NULL, -- foreign key
+	Serial_Port boolean NOT NULL,
+	Networked boolean NOT NULL, 
+  	PRIMARY KEY (CNC_Key)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='CNC info';
+insert into CNC (CNC_Key,CNC,CNC_Type_Key,Serial_Port,Networked)
+values (1,'103',1,true,false)
+
+select * from CNC
+
+
+/*
+ * Many CNC can be assigned to 1 work center
+ */
 -- drop table CNC_Workcenter
 CREATE TABLE CNC_Workcenter (
-	CNC varchar(6),
-  	Workcenter_Key int NOT NULL, 
-  	PRIMARY KEY (CNC)
+	CNC_Workcenter_Key int NOT NULL,
+	CNC_Key int NOT NULL,  -- foreign key
+  	Workcenter_Key int NOT NULL, -- foreign key
+  	PRIMARY KEY (CNC_Workcenter_Key)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='Link CNC to a workcenter';
-insert into CNC_Workcenter (CNC,Workcenter_Key)
-values ('103',2809196) 
+insert into CNC_Workcenter (CNC_Key,Workcenter_Key)
+values (1,2809196) 
 
 select * from CNC_Workcenter
 /*
  * Tool List master table 
  */
 
+/*
+ * Corresponds to Plex part_v_part
+ */
 -- drop table Part
 -- truncate table Part
 CREATE TABLE Part (
@@ -42,20 +79,9 @@ insert into Part (Part_Key,Part_No,Revision,Name,Part_Type)
 values (2809196,'51393 TJB A040M1','40-M1-','RDX Right Hand','Bracket') 
 select * from Part
 
--- drop table Part_Operation
--- truncate table Part_Operation
-CREATE TABLE Part_Operation (
-	Part_Operation_Key	int NOT NULL,
-	Part_Key int NOT NULL,
-	Operation_No int NOT NULL,
-	Operation_Key int NOT NULL,
-	PRIMARY KEY (Part_Operation_Key)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='A subset of fields from Plex part_v_part_operation';
-select * from Part_Operation
-
-insert into Part_Operation (Part_Operation_Key,Part_Key,Operation_No,Operation_Key)
-values (7914696,2809196,100,56409)
-
+/*
+ * Corresponds to plex part_v_operation
+ */
 -- drop table Operation
 -- truncate table Operation
 CREATE TABLE Operation (
@@ -68,8 +94,39 @@ values (56409,'Machine Complete')
 
 select * from Operation
 
+
 /*
- * This corresponds to a Plex tool assembly
+ * Corresponds to plex part_v_part operation
+ */
+-- drop table Part_Operation
+-- truncate table Part_Operation
+CREATE TABLE Part_Operation (
+	Part_Operation_Key	int NOT NULL,
+	Part_Key int NOT NULL,
+	Operation_Key int NOT NULL,
+	PRIMARY KEY (Part_Operation_Key)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='A subset of fields from Plex part_v_part_operation';
+insert into Part_Operation (Part_Operation_Key,Part_Key,Operation_Key)
+values (7914696,2809196,56409)
+select * from Part_Operation
+
+
+-- drop table CNC_Part_Operation
+-- truncate table CNC_Part_Operation
+CREATE TABLE CNC_Part_Operation (
+	CNC_Part_Operation_Key int NOT NULL,
+	CNC_Key int NOT NULL,
+	Part_Key int NOT NULL,
+	Operation_Key int NOT NULL,
+	PRIMARY KEY (CNC_Part_Operation_Key)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='Links CNC to a part operation';
+insert into CNC_Part_Operation (CNC_Part_Operation_Key,CNC_Key,Part_Key,Operation_Key)
+values(1,1,2809196,56409)
+select * from CNC_Part_Operation 
+
+/*
+ * This corresponds to a Plex tool assembly.
+ * ASSEMBLY KEY WILL NEED TO BE CHANGED TO ACTUAL VALUE ONCE UPLOAD IS COMPLETE
  */
 -- drop table Tool_Assembly
 -- truncate table Tool_Assembly
@@ -80,54 +137,45 @@ CREATE TABLE Tool_Assembly (
   	PRIMARY KEY (Assembly_Key)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='A subset of fields from Plex part_v_Tool_Assembly';
 insert into Tool_Assembly (Assembly_Key,Assembly_No,Description)
--- values (1,'T12','86.125MM PRE FINISH BORE')
--- values (2,'T01','85.24MM ROUGH BORE')
--- values (3,'T10','86.925MM FINISH BORE')
--- values (4,'T11','ETCHER')
--- values (5,'T13','180MM BACK CUTTER RH PART ONLY')
--- values (6,'T16','135MM BACK CUTTER RH ONLY')
--- values (7,'T07','8.2MM DRILL')
--- values (8,'T08','14.3MM DRILL/CHAMFER')
--- values (9,'T09','15.5MM DRILL/CHAMFER')
--- values (10,'T04','21MM DRILL/SPOTFACE')
--- values (11,'T05','10MM END MILL')
--- values (12,'T02','1.25\" HELICAL MILL')
-
+-- values (1,'T10','86.575MM FINISH BORE')
+-- values (2,'T11','ETCHING TOOL')
+-- values (3,'T01','85.24MM ROUGH BORE')
+-- values (4,'T12','86.125MM ROUGH BORE')
+-- values (5,'T02','1.25" HELLICAL MILL')
+-- values (6,'T13','180MM DISK MILL')
+-- values (7,'T04','21MM DRILL')
+-- values (8,'T05','10MM END MILL')
+-- values (9,'T16','135MM DISK MILL')
+-- values (10,'T07','8.2MM DRILL')
+-- values (11,'T08','14.3MM DRILL')
+-- values (12,'T09','15.5MM DRILL')
 select * from Tool_Assembly 
 
-
-
-/*
- * CNC info, don't want to have a varchar primary_key
- */
--- drop table CNC 
-CREATE TABLE CNC (
-	CNC_Key int NOT NULL,
-	CNC varchar(10) NOT NULL,
-  	PRIMARY KEY (CNC_Key)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='CNC unique identifier';
-insert into CNC (CNC_Key,CNC)
-values (1,'103')
-
-select * from CNC
-/*
- * CNC tool life for assembly from TLM.SSB
- */
--- drop table CNC_Tool_Tracker 
-CREATE TABLE CNC_Tool_Tracker (
-	CNC_Tool_Tracker_Key int NOT NULL,
-	CNC_Key int NOT NULL,
+-- drop table Tool_Assembly_Part
+-- truncate table Tool_Assembly_Part
+-- NEEDS UPDATED AFTER PLEX TOOLING MODULE UPLOAD
+CREATE TABLE Tool_Assembly_Part (
+	Tool_Assembly_Part_Key int NOT NULL,
 	Part_Key int NOT NULL,
 	Operation_Key int NOT NULL,
 	Assembly_Key int NOT NULL, 
-	Tool_Life int NOT NULL,  -- Can be different for every CNC
-  	Current_Value int NOT NULL,
-  	Last_Update datetime NOT NULL,
-  	PRIMARY KEY (CNC_Tool_Tracker_Key)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='CNC tool tracker info';
+  	PRIMARY KEY (Tool_Assembly_Part_Key)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='A subset of fields from Plex part_v_Tool_Assembly_Part';
+insert into Tool_Assembly_Part (Tool_Assembly_Part_Key,Part_Key,Operation_Key,Assembly_Key)
+-- values (1,2809196,56409,1)
+-- values (2,2809196,56409,2)
+-- values (3,2809196,56409,3)
+-- values (4,2809196,56409,4)
+-- values (5,2809196,56409,5)
+-- values (6,2809196,56409,6)
+-- values (7,2809196,56409,7)
+-- values (8,2809196,56409,8)
+-- values (9,2809196,56409,9)
+-- values (10,2809196,56409,10)
+-- values (11,2809196,56409,11)
+-- values (12,2809196,56409,12)
+select * from Tool_Assembly_Part
 
-set @Last_Update = '2020-08-15 00:00:00';
-insert into CNC_Tool_Tracker (CNC_Tool_Tracker_Key,CNC_Key,Part_Key,Assembly_Key,Tool_Life,Current_Value,Last_Update)
 -- values (1,1,2809196,1,73000,-1,@Last_Update )
 -- values (2,1,2809196,2,52600,-1,@Last_Update)
 -- values (3,1,2809196,3,80500,-1,@Last_Update)
@@ -142,23 +190,74 @@ insert into CNC_Tool_Tracker (CNC_Tool_Tracker_Key,CNC_Key,Part_Key,Assembly_Key
 -- values (12,1,2809196,12,40000,-1,@Last_Update)
 
 
--- values (1,'T12','86.125MM PRE FINISH BORE')
--- values (2,'T01','85.24MM ROUGH BORE')
--- values (3,'T10','86.925MM FINISH BORE')
--- values (4,'T11','ETCHER')
--- values (5,'T13','180MM BACK CUTTER RH PART ONLY')
--- values (6,'T16','135MM BACK CUTTER RH ONLY')
--- values (7,'T07','8.2MM DRILL')
--- values (8,'T08','14.3MM DRILL/CHAMFER')
--- values (9,'T09','15.5MM DRILL/CHAMFER')
--- values (10,'T04','21MM DRILL/SPOTFACE')
--- values (11,'T05','10MM END MILL')
--- values (12,'T02','1.25\" HELICAL MILL')
+
+/*
+ * UDP Datagrams sent from Moxa units.
+ * Common variablies used as assembly counters are identified by an CNC_Part_Operation_Key, Set_No, and Block_No 
+ * when sent to the UDP server.  This table links the assembly counter value to a Tool_Assembly_Part record.
+ */
+-- UPDATE ASSEMBLY KEY AFTER TOOLING UPLOAD
+-- drop table CNC_Part_Operation_Set_Block 
+CREATE TABLE CNC_Part_Operation_Set_Block (
+	CNC_Part_Operation_Set_Block int NOT NULL,
+	CNC_Part_Operation_Key int NOT NULL,  -- each Datagram_Key, Set_No, Block_No combination maps to 1 CNC, Part, Part_Operation, Assembly_Key pair.
+	Set_No int NOT NULL,  -- Can't avoid this Set_No because of the way the Moxa receives messages from the Okuma's serial port.
+	Block_No int NOT NULL,  -- This is just an index to identify which 10-byte block in a datagram set. 
+	Assembly_Key int NOT NULL, -- foreign key
+  	PRIMARY KEY (CNC_Part_Operation_Set_Block)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='UDP Datagrams sent from Moxa units';
+
+insert into CNC_Part_Operation_Set_Block (CNC_Part_Operation_Set_Block,CNC_Part_Operation_Key,Set_No,Block_No,Assembly_Key)
+-- values (1,1,1,1,1)
+-- values (2,1,1,2,2)
+-- values (3,1,1,3,3)
+-- values (4,1,1,4,4)
+-- values (5,1,1,5,5)
+-- values (6,1,1,6,6)
+-- values (7,1,1,7,7)
+-- values (8,1,1,8,8)
+-- values (9,1,1,9,9)
+-- values (10,1,2,1,10)
+-- values (11,1,2,2,11)
+-- values (12,1,2,3,12)
+select * from CNC_Part_Operation_Set_Block
+/*
+ * CNC tool life for assembly from TLM.SSB
+ */
+-- drop table CNC_Tool_Tracker 
+-- truncate table CNC_Tool_Tracker
+CREATE TABLE CNC_Tool_Tracker (
+	CNC_Tool_Tracker_Key int NOT NULL,
+	CNC_Key int NOT NULL,
+	Part_Key int NOT NULL,
+	Operation_Key int NOT NULL,
+	Assembly_Key int NOT NULL, 
+	Tool_Life int NOT NULL,  -- Can be different for every CNC
+  	Current_Value int NOT NULL,
+  	Last_Update datetime NOT NULL,
+  	PRIMARY KEY (CNC_Tool_Tracker_Key)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='CNC tool tracker info';
+
+set @Last_Update = '2020-08-15 00:00:00';
+insert into CNC_Tool_Tracker (CNC_Tool_Tracker_Key,CNC_Key,Part_Key,Assembly_Key,Tool_Life,Current_Value,Last_Update)
+-- values (1,1,2809196,1,80500,-1,@Last_Update )
+-- values (2,1,2809196,2,130000,-1,@Last_Update)
+-- values (3,1,2809196,3,52600,-1,@Last_Update)
+-- values (4,1,2809196,4,73000,-1,@Last_Update)
+-- values (5,1,2809196,5,40000,-1,@Last_Update)
+-- values (6,1,2809196,6,999999,-1,@Last_Update)
+-- values (7,1,2809196,7,72000,-1,@Last_Update)
+-- values (8,1,2809196,8,50000,-1,@Last_Update)
+-- values (9,1,2809196,9,999999,-1,@Last_Update)
+-- values (10,1,2809196,10,110000,-1,@Last_Update)
+-- values (11,1,2809196,11,100000,-1,@Last_Update)
+-- values (12,1,2809196,12,110000,-1,@Last_Update)
+
+select * from CNC_Tool_Tracker
 
 
-
-update CNC_Tool_Tracker 
-set Tool_Life = 50000 
+--update CNC_Tool_Tracker 
+--set Tool_Life = 50000 
 -- set Tool_Life = 50000, Last_Update = '2020-08-15 00:00:00' 
 where CNC_Key = 1 and Part_Key = 2809196 and Assembly_Key = 11
 
@@ -245,27 +344,3 @@ select * from ToolTracker
 set @transDate = '2020-06-25 00:00:00';
 call InsToolLife(103,12345, 100, @transDate); 
 select * from ToolLife where cnc=103 order by CNC, desc
--- drop procedure InsKep13319
-CREATE PROCEDURE InsKep13319
-(
-  	IN nodeId varchar(50),
-  	IN name varchar(50),
-  	IN plexus_Customer_No int(11),
-	IN pcn varchar(50),
-  	IN workcenter_Key int(11),
-  	IN workcenter_Code varchar(50),
-  	IN cnc varchar(6),
-  	IN value int(11),
-  	IN transDate datetime
-)
-BEGIN
-   
-INSERT INTO Kep13319 
-(nodeId,name,plexus_Customer_No,pcn, workcenter_Key,workcenter_Code,cnc,value,transDate)
-VALUES(nodeId,name,plexus_Customer_No,pcn, workcenter_Key,workcenter_Code,cnc,value,transDate);
-
--- Display the last inserted row.
-select kep13319Key, workcenter_Key from Kep13319 k where Kep13319Key=(SELECT LAST_INSERT_ID());
-
-END;
-
